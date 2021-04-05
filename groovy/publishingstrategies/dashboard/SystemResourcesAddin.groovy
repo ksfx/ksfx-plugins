d60da52410e7f79939d69cbb3898de1259064434
Kejo Starosta <k@ksfx.de>
2021-04-05 23:12:47 +0200

    Dashboard Plugin

--- groovy/publishingstrategies/dashboard/SystemResourcesAddin.groovy
+++ groovy/publishingstrategies/dashboard/SystemResourcesAddin.groovy
@@ -15,6 +15,9 @@
  * limitations under the License.
  */
 
+
+import ch.ksfx.dao.GenericDataStoreDAO
+import ch.ksfx.model.GenericDataStore
 import ch.ksfx.model.publishing.PublishingStrategy
 import ch.ksfx.model.publishing.PublishingSharedData
 import ch.ksfx.services.ServiceProvider
@@ -26,10 +29,14 @@ import ch.ksfx.util.PublishingDataShare
 class DashboardSystemResources implements PublishingStrategy
 {
     private ServiceProvider serviceProvider;
+    private GenericDataStoreDAO genericDataStoreDAO;
+
+    private Long megaByte = 1024l * 1024l;
 
     public DashboardSystemResources(ServiceProvider serviceProvider)
     {
         this.serviceProvider = serviceProvider;
+        this.genericDataStoreDAO = serviceProvider.getService(GenericDataStoreDAO.class);
     }
 
     public GenericResponse getPublishingData()
@@ -37,8 +44,63 @@ class DashboardSystemResources implements PublishingStrategy
         IndexService indexService = serviceProvider.getService(IndexService.class);
 
         StringBuilder sb = new StringBuilder();
-        sb.append("Current Index Queue Size: " + indexService.getCurrentQueueSize());
+        sb.append("<div class='list-group'>");
+            sb.append("<div class='list-group-item'>");
+                sb.append("Series stats:").append("<br />");
+                sb.append("Total series: ").append(getGenericDataStoreInformation("totalSeriesCount")).append("<br />");
+                sb.append("Total Observations: ").append(getGenericDataStoreInformation("totalObservationsCount")).append("<br />");
+            sb.append("</div>");
+            sb.append("<div class='list-group-item'>");
+                sb.append("Memory stats:").append("<br />");
+                sb.append("Free memory: ").append(getFreeMemory()).append("<br />");
+                sb.append("Used memory: ").append(getUsedMemory()).append("<br />");
+                sb.append("Total memory: ").append(getTotalMemory()).append("<br />");
+                sb.append("Max memory: ").append(getMaxMemory()).append("<br />");
+            sb.append("</div>");
+            sb.append("<div class='list-group-item'>");
+                sb.append("Current Index Queue Size: " + indexService.getCurrentQueueSize());
+            sb.append("</div>");
+        sb.append("</div>");
 
         return new GenericResponse(sb.toString().getBytes("UTF-8"), "", "text/html", false);
+    }
+
+    public Long getFreeMemory()
+    {
+        Runtime runtime = Runtime.getRuntime();
+
+        return (runtime.freeMemory() / megaByte);
+    }
+
+    public Long getUsedMemory()
+    {
+        Runtime runtime = Runtime.getRuntime();
+
+        return ((runtime.totalMemory() - runtime.freeMemory()) / megaByte);
+    }
+
+    public Long getTotalMemory()
+    {
+        Runtime runtime = Runtime.getRuntime();
+
+        return (runtime.totalMemory() / megaByte);
+    }
+
+    public Long getMaxMemory()
+    {
+        Runtime runtime = Runtime.getRuntime();
+
+        return (runtime.maxMemory() / megaByte);
+    }
+
+    public String getGenericDataStoreInformation(String key)
+    {
+        GenericDataStore genericDataStore = genericDataStoreDAO.getGenericDataStoreForKey(key);
+
+        if (genericDataStore == null) {
+            return "";
+        }
+
+        return genericDataStore.getDataValue();
     }
 }
